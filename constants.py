ddf10fcf563c841038c10f45fa7e2c774d8a9959
Hamza Khan <63538732+hamza276@users.noreply.github.com>
2024-11-25 07:56:50 -0800

    constants.py

--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
-API_Key="..............Your Own Groq API.............."  # https://console.groq.com/playground?model=llama3-8b-8192
+API_Key="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" # Dummy API  # https://console.groq.com/playground?model=llama3-8b-8192
 WORD_LIMIT = 2700
